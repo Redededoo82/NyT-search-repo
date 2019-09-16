d00ddd8ca978535161793fdd050d9893ac017f53
William Lukas <iggyboy456@gmail.com>
2019-09-16 13:31:00 -0500

    added date soorting and sorting rseults by relevance

--- api.js
+++ api.js
@@ -1,7 +1,10 @@
 
 let query = prompt("enter query");
+let minDate = prompt("enter min pub year");
+let maxDate = prompt("enter max pub year");
 
-newurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&api-key=eknM4Z7Rm1Z1fL6mj81V5sn18rQehL6H";
+
+newurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&begin_date=" + minDate + "0101&end_date=" + maxDate + "1231&sort=relevance" + "&api-key=eknM4Z7Rm1Z1fL6mj81V5sn18rQehL6H";
 
 $.ajax({
     url: newurl,
